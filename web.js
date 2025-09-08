ddcaba780064cd374092ec65c3add9b09605daea
moonjongjs <moonseonjong@naver.com>
2025-09-09 07:45:28 +0900

    SSR deploy with standalone build

--- web.js
+++ web.js
@@ -2,17 +2,15 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const dev = false;
 const port = process.env.PORT || 8001;
-const app = next({ dev });
+const app = next({ dev: false, dir: "." }); // 현재 디렉토리 기준 실행
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
+  }).listen(port, () => {
     console.log(`✅ Next.js SSR running on port ${port}`);
   });
 });
